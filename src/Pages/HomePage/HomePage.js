305c978b201d349159347b309eb53ed392bd8c20
marcusluis <marcusluis.mail@gmail.com>
2021-10-19 12:47:48 -0300

    commit

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,12 +1,31 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import useRequestData from "../../hooks/useRequestData";
 import PokeCard from "../../components/PokeCard/PokeCard";
+import { BASE_URL } from "../../constantes/url";
 
 const HomePage = () => {
+  const pokeData = useRequestData({}, `${BASE_URL}/pokemon?limit=0&offset=0`);
+  console.log(pokeData);
+
+  const getAllPokemons =
+    pokeData.results &&
+    pokeData.results.map((item, index) => {
+      // const pokeImg = useRequestData({}, pokeData[index]);
+      // console.log(pokeImg);
+      return (
+        <div key={index}>
+          <p>name={item.name}</p>
+          {/* <img src={item.url.sprites.front_default} /> */}
+        </div>
+      );
+    });
+
   return (
     <div>
       <Header />
       <PokeCard />
+      {getAllPokemons}
     </div>
   );
 };
