9e3f2e81c130233e98f1451d971847b194371f9c
GeisyLopes <geisylopes7l@gmail.com>
2021-10-18 21:49:07 -0300

    esqueleto detalhes

--- src/Pages/DetailPage/DetailPage.js
+++ src/Pages/DetailPage/DetailPage.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Titulo, OnCard, Card1, Card2, Card3, Card4 } from "./styled";
 
 const DetailPage = () => {
-  return <di>DetailPage</di>;
+  return (
+    <div>
+      <Titulo>Detalhes</Titulo>
+      <div>
+        <OnCard>
+          Imagens
+          {/* <Card1>
+            <img></img>
+          </Card1> */}
+
+          <Card2>
+            Eu sou uma imagem
+            <img></img>
+          </Card2>
+        </OnCard>
+
+        <Card3>Poderes</Card3>
+
+        <Card4>
+          <h2></h2>
+          <div>Principais Ataques</div>
+        </Card4>
+      </div>
+    </div>
+  );
 };
 
 export default DetailPage;
---
+++ src/Pages/DetailPage/styled.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const Titulo = styled.h1`
+    color:orangered;
+`
+export const OnCard = styled.a `
+    display: flex;
+    justify-content: left;
+    justify-items: left;
+    background-color: #35b0e8;
+    box-shadow: 5px 5px 10px 10px red;
+    border-radius: 10px;
+    padding: 0px 20px 100px;
+    margin-top: 50px;
+    width: 50%;
+    left: 100%;
+    height: 100%;
+`
+export const Card2 = styled.a `
+    display: flex;
+    justify-content: left;
+    justify-items: left;
+    background-color: #FF9933;
+    box-shadow: 10px 10px 10px #727D71;
+    border-radius: 10px;
+    padding: 0px 20px 100px;
+    margin-top: 30px;
+    width: 50%;
+    left: 100%;
+    height: 100%;
+`
+export const Card3 = styled.a `
+    display: flex;
+    justify-content: left;
+    justify-items: left;
+    background-color: #FF9933;
+    box-shadow: 10px 10px 10px #727D71;
+    border-radius: 10px;
+    padding: 0px 20px 100px;
+    margin-top: 30px;
+    width: 50%;
+    left: 100%;
+    height: 100%;
+`
+export const Card4 = styled.a `
+    display: flex;
+    justify-content: left;
+    justify-items: left;
+    background-color: #FF9933;
+    box-shadow: 10px 10px 10px #727D71;
+    border-radius: 10px;
+    padding: 0px 20px 100px;
+    margin-top: 30px;
+    width: 50%;
+    left: 100%;
+    height: 100%;
+`
