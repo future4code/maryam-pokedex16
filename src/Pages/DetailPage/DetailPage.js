209e0a4758bb64e24fb3fd710921dde8f86d929d
GeisyLopes <geisylopes7l@gmail.com>
2021-10-20 09:39:19 -0300

    configurando

--- src/Pages/DetailPage/DetailPage.js
+++ src/Pages/DetailPage/DetailPage.js
@@ -1,39 +1,31 @@
 import React from "react";
-import {
-  Titulo,
-  OnCard,
-  Card1,
-  Card2,
-  Card3,
-  Card4,
-  BotaoVoltar,
-} from "./styled";
+import {  Titulo,  OnCard,  Card2,  Card3,  Card4,  BotaoVoltar,} from "./styled";
+import { useHistory } from "react-router";
+import { goToPokedexPage } from "../../Router/Coordinator";
+import Header from "../../components/Header/Header";
 
 const DetailPage = () => {
+  
+  const history = useHistory();
   return (
     <div>
       <Titulo>Detalhes</Titulo>
-      <BotaoVoltar onClick={() => this.props.changePage("PokedexPage")}>
+      <BotaoVoltar onClick={() => goToPokedexPage(history)}>
         Voltar para Pokedex
       </BotaoVoltar>
       <div>
         <OnCard>
-          Imagens
-          {/* <Card1>
-            <img></img>
-          </Card1> */}
           <Card2>
-            Eu sou uma imagem
-            <img></img>
+         
           </Card2>
-        </OnCard>
-
         <Card3>Poderes</Card3>
 
         <Card4>
           <h2></h2>
           <div>Principais Ataques</div>
         </Card4>
+        </OnCard>
+
       </div>
     </div>
   );
--- src/Pages/DetailPage/styled.js
+++ src/Pages/DetailPage/styled.js
@@ -1,67 +1,70 @@
 import styled from "styled-components";
 
 export const Titulo = styled.h1`
-    color:orangered;
+    background-color: orange;
+    color:#35b0e8;
 `
 export const OnCard = styled.a `
     display: flex;
-    justify-content: left;
-    justify-items: left;
     background-color: #35b0e8;
-    box-shadow: 5px 5px 10px 10px red;
-    border-radius: 10px;
-    padding: 0px 20px 100px;
-    margin-top: 50px;
-    width: 50%;
-    left: 100%;
-    height: 100%;
+    width: 50px;
+    left: 300px;
+    height: 200px;
+    margin-left: 180px;
 `
 export const Card2 = styled.a `
     display: flex;
     justify-content: left;
     justify-items: left;
     background-color: #FF9933;
-    box-shadow: 10px 10px 10px #727D71;
     border-radius: 10px;
-    padding: 0px 20px 100px;
-    margin-top: 30px;
-    width: 50%;
-    left: 100%;
-    height: 100%;
+    padding: 180px 80px 100px;
+    margin-top: 50px; 
+    max-width: 50px;
+    width: 50px;
+    left: 100px;
+    height: 400px;
+    align-items: center;
+    background-color: #FF9933;
+    margin-inline-end: 20px;
 `
 export const Card3 = styled.a `
     display: flex;
-    justify-content: left;
-    justify-items: left;
+    justify-content: center;
+    justify-items: center;
     background-color: #FF9933;
-    box-shadow: 10px 10px 10px #727D71;
+    margin-top: 50px; 
+    align-items: center;
     border-radius: 10px;
-    padding: 0px 20px 100px;
-    margin-top: 30px;
-    width: 50%;
-    left: 100%;
-    height: 100%;
+    padding: 130px 200px 100px;
+    width: 50px;
+    left: 100px;
+    height: 400px;
+    margin-inline-end: 20px;
 `
 export const Card4 = styled.a `
     display: flex;
-    justify-content: left;
+    justify-content: center;
     justify-items: left;
     background-color: #FF9933;
-    box-shadow: 10px 10px 10px #727D71;
     border-radius: 10px;
-    padding: 0px 20px 100px;
-    margin-top: 30px;
-    width: 50%;
-    left: 100%;
-    height: 100%;
+    padding: 130px 200px 100px;
+    margin-top: 50px;
+    width: 50px;
+    left: 100px;
+    height: 400px;
+    margin-inline-end: 20px;
 `
+
 export const BotaoVoltar = styled.button`
-    margin: 0 auto;
-    margin-bottom: 20px;
+    margin: 10px;
+    margin-bottom: 30px;
+    border-radius: 32px;
     width: 200px;
     height: 30px;
     background-color: #FF9933;
     font-weight: bold;
     font-weight: 800;
     transition: background-color 1s, color 1s, box-shadow 1s;
+    margin-left: 20px;
 `
